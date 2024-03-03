39e1cfc389b6323e44baa3c94b5abaec873f08fd
faridguzman <farid.guzman91@hotmail.com>
2024-03-03 22:46:02 +0100

    added vite config, added orbit controls

--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,41 @@
 import * as THREE from 'three';
+import { REVISION } from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js'
+import gsap from 'gsap'
+import GUI from 'lil-gui'
 
-const width = window.innerWidth, height = window.innerHeight;
 
 // init
 
-const camera = new THREE.PerspectiveCamera( 70, width / height, 0.01, 10 );
-camera.position.z = 1;
+const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 10000);
+camera.position.set(0, 1, 1)
 
 const scene = new THREE.Scene();
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setAnimationLoop(animation);
+document.body.appendChild(renderer.domElement);
+const controls = new OrbitControls(camera, renderer.domElement)
+const isPlaying = true;
+const gui = new GUI()
 
-const geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
+controls.update()
+
+const geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
 const material = new THREE.MeshNormalMaterial();
 
-const mesh = new THREE.Mesh( geometry, material );
-scene.add( mesh );
-
-const renderer = new THREE.WebGLRenderer( { antialias: true } );
-renderer.setSize( width, height );
-renderer.setAnimationLoop( animation );
-document.body.appendChild( renderer.domElement );
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
 
 // animation
 
-function animation( time ) {
-
-	mesh.rotation.x = time / 2000;
-	mesh.rotation.y = time / 1000;
-
-	renderer.render( scene, camera );
+function animation(time: any) {
+  requestAnimationFrame(animation);
+   mesh.rotation.x = time / 2000; 
+   mesh.rotation.y = time / 1000;
+/* 	controls.update(); */
+  renderer.render(scene, camera);
 
 }
